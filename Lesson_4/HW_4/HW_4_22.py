f5e1c9092db5c20928ef7b5f2ab177dbe6a613ed
Mayya <majko2594@gmail.com>
2023-06-01 10:12:58 +0300

    modified HW 4_22

--- Lesson_4/HW_4/HW_4_22.py
+++ Lesson_4/HW_4/HW_4_22.py
@@ -16,7 +16,15 @@ list_2 = []
 for i in range(m):
     list_2.append(random.randint(1, 100))
 print(list_2)
-result = set(list_1).union(set(list_2))
+result = set(list_1).intersection(set(list_2))
 list_res = list(result)
 list_res.sort()
+
+# for i in range(len(list_res)):
+#   for j in range(i + 1, len(kool)):
+    #   if list_res[i] > list_res[i + 1]:
+    #      temp = list_res[i]
+    #      list_res[i] = list_res[i + 1]
+    #      list_res[i + 1] = temp)
+
 print(list_res)
